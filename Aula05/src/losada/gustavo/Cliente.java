492075ce8d949d5fc675f54d22608c19b50d66f2
GusLos <79462583+GusLos@users.noreply.github.com>
2021-03-15 11:39:52 -0300

    Criação da aula5

---
+++ Aula05/src/losada/gustavo/Cliente.java
@@ -0,0 +1,15 @@
+package losada.gustavo;
+
+public class Cliente {
+    String titular;
+    String cpf;
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "titular='" + titular + '\'' +
+                ", cpf='" + cpf + '\'' +
+                '}';
+    }
+
+}
