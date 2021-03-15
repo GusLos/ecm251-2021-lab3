8a3d781cfaf4eaa589c66fddf6d2562a19607b12
GusLos <79462583+GusLos@users.noreply.github.com>
2021-03-15 13:11:47 -0300

    Aula05

--- Aula05/src/losada/gustavo/Conta.java
+++ Aula05/src/losada/gustavo/Conta.java
@@ -1,21 +1,26 @@
 package losada.gustavo;
 
 public class Conta {
-    int numero;
-    double saldo;
-    Cliente cliente;
-    
-    void visualizarSaldo(){
-        //double saldo = 10;
-        System.out.println("Saldo da conta: R$" + this.saldo);
+    private int numero;
+    private double saldo;
+    private Cliente cliente;
+
+    public Conta(int numero, double saldo, String cliente) {
+        this.numero = numero;
+        this.saldo = saldo;
+        this.cliente = new Cliente(cliente);
     }
-    void depositar(double valor){
+
+    public double getSaldo(){
+        return this.saldo;
+    }
+    public void depositar(double valor){
         //this.saldo = this.saldo + valor;
         this.saldo += saldo;
     };
 
 
-    boolean sacar(double valor) {
+    public boolean sacar(double valor) {
         if (this.saldo >= valor) {
             this.saldo -= valor;
             return true;
@@ -23,7 +28,7 @@ public class Conta {
         return false;
     };
 
-    boolean transferirPara(double valor, Conta destino) {
+    public boolean transferirPara(double valor, Conta destino) {
         if (this.sacar(valor)){
             destino.depositar(valor);
             return true;
