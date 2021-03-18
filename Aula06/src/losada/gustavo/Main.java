9a5943af40cbb32d0d5230a5f1886217f173f379
GusLos <79462583+GusLos@users.noreply.github.com>
2021-03-18 18:08:33 -0300

    Aula05

---
+++ Aula06/src/losada/gustavo/Main.java
@@ -0,0 +1,9 @@
+package losada.gustavo;
+
+public class Main {
+
+    public static void main(String[] args) {
+            String cpf = "491.842.308-60";
+            System.out.println("Cpf valido:" + ValidadorDeCPF.validarCpf(cpf));
+    }
+}
