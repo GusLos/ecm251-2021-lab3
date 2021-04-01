2af4bf52ff8c7d44f90a79e623b0b212f9d368a6
GusLos <79462583+GusLos@users.noreply.github.com>
2021-04-01 17:28:13 -0300

    Menu básico
    Menu básico criado na classe Atividade1,
    feito revendo as aulas gravadas (Aula04 Lab3).

--- Atividade1/src/losada/gustavo/Atividade1.java
+++ Atividade1/src/losada/gustavo/Atividade1.java
@@ -1,4 +1,44 @@
 package losada.gustavo;
+import java.util.Scanner;
 
 public class Atividade1 {
+    private boolean continuarExecucao;
+    private Scanner scanner;
+
+    public Atividade1(){
+        this.continuarExecucao = true;
+        this.scanner = new Scanner(System.in);
+    }
+
+    public void executar(){
+        while(continuarExecucao) {
+            exibirMenu();
+            int opcao = scanner.nextInt();
+            avaliarOpcao(opcao);
+        }
+    }
+
+    private void avaliarOpcao (int opcao){
+        switch (opcao){
+            case 0:
+                this.continuarExecucao = false;
+                break;
+            case 1:
+                System.out.println("Função não implementada");
+                break;
+            case 2:
+                System.out.println("Função não implementada");
+                break;
+            default :
+                System.out.println("Opção invalida");
+                break;
+        }
+    }
+
+    private void exibirMenu(){
+        System.out.println("Menu:");
+        System.out.println("1 - Pagar");
+        System.out.println("2 - Receber");
+        System.out.println("0 - Fim");
+    }
 }
--- Atividade1/src/losada/gustavo/Main.java
+++ Atividade1/src/losada/gustavo/Main.java
@@ -3,6 +3,7 @@ package losada.gustavo;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	Atividade1 atividade = new Atividade1();
+    atividade.executar();
     }
 }
