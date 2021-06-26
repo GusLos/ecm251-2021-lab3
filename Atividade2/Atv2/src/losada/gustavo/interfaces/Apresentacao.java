92ceb594f46fffcb6eeb01ed846cd11db226bac1
GusLos <79462583+GusLos@users.noreply.github.com>
2021-06-26 10:10:14 -0300

    Inicio da interface Apresentação

---
+++ Atividade2/Atv2/src/losada/gustavo/interfaces/Apresentacao.java
@@ -0,0 +1,8 @@
+package losada.gustavo.interfaces;
+
+/**
+ * Interface que os membros devem implementar para poder se apresentar quando for necessario.
+ */
+public interface Apresentacao {
+    public String apresentar();
+}
