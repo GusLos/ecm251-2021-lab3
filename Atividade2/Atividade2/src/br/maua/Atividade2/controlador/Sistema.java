c8c2caf742014037c3ca0267131b4e05ad719ded
GusLos <79462583+GusLos@users.noreply.github.com>
2021-06-26 17:36:12 -0300

    Principais métodos e parâmetros para Sistema
    >Passando principais parâmetros e métodos da classe Sistema de Atv2 para Atividade2

--- Atividade2/Atividade2/src/br/maua/Atividade2/controlador/Sistema.java
+++ Atividade2/Atividade2/src/br/maua/Atividade2/controlador/Sistema.java
@@ -1,8 +1,27 @@
 package br.maua.Atividade2.controlador;
 
+import br.maua.Atividade2.models.BigBrothers;
+
+import java.util.Scanner;
+
 /**
  * Classe que faz todas as interações.
  */
 public class Sistema {
-    
+    private static SistemaHorario horario;
+    private Scanner scanner;
+    private BigBrothers bigBrother;
+
+    public class Sistema(){}
+
+    public void run(){}
+
+    private int menu(){}
+
+    private void analisarOpcao(int opcao){}
+
+    private void registrarMembro(){}
+
+    private void postarMensagem(){}
+
 }
