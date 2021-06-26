8a1673465004eefab808491e8a055b2acc0e4f53
GusLos <79462583+GusLos@users.noreply.github.com>
2021-06-26 09:40:12 -0300

    Descrição na classe Membros

--- Atividade2/Atv2/src/losada/gustavo/models/Membros.java
+++ Atividade2/Atv2/src/losada/gustavo/models/Membros.java
@@ -3,7 +3,10 @@ package losada.gustavo.models;
 import losada.gustavo.enums.TiposMembros;
 
 /**
- * Classe abstrata Membros possui o que os membros tem em comum ().
+ * Classe abstrata Membros possui o que os membros tem em comum:
+ * nome - nome do usuario;
+ * eMail - e-mail do usuario;
+ * funcao - função ou o tipo de membro que ele é.
  */
 public abstract class Membros {
     private String nome;
