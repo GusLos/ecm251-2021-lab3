d2d044276c71e5e52fb39a1b12f79f36dffd5c5e
GusLos <79462583+GusLos@users.noreply.github.com>
2021-06-26 17:44:05 -0300

    Enum TipoMembros

--- Atividade2/Atividade2/src/br/maua/Atividade2/enums/TiposMembros.java
+++ Atividade2/Atividade2/src/br/maua/Atividade2/enums/TiposMembros.java
@@ -1,4 +1,12 @@
 package br.maua.Atividade2.enums;
 
+/**
+ * Enum para os tipos de membros da sociedade, sendo:
+ * MOBILE_MEMBERS - gerenciam postagens em redes sociais;
+ * HEAVY_LIFTERS - responsáveis por manter os repositórios;
+ * SCRIPT_GUYS - fazem os primeiros contatos com novos clientes;
+ * BIG_BROTHERS - responsáveis por monitorar se os demais membros.
+ */
 public enum TiposMembros {
+    MOBILE_MEMBERS, HEAVY_LIFTERS, SCRIPT_GUYS, BIG_BROTHERS;
 }
