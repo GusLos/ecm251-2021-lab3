e184c4266d095f594ae865a9e7c56577714e234a
GusLos <79462583+GusLos@users.noreply.github.com>
2021-06-26 17:42:49 -0300

    Enum HorarioSistema

--- Atividade2/Atividade2/src/br/maua/Atividade2/enums/HorarioSistema.java
+++ Atividade2/Atividade2/src/br/maua/Atividade2/enums/HorarioSistema.java
@@ -1,4 +1,10 @@
 package br.maua.Atividade2.enums;
 
+/**
+ * Enum para os horários do sistema, sendo:
+ * NORMAL - horário de atividades padrões para todos os membros;
+ * EXTRA - horário para as atividades "extras".
+ */
 public enum HorarioSistema {
+    NORMAL, EXTRA;
 }
