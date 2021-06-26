d954b8a6172cf54131a7d68d2e7da517e5d08eca
GusLos <79462583+GusLos@users.noreply.github.com>
2021-06-26 18:33:29 -0300

    Classe para testes
    >Onde testo tudo antes de colocar em outro lugar.

---
+++ Atividade2/Atividade2/src/br/maua/Atividade2/teste_drive/TesteDrive.java
@@ -0,0 +1,33 @@
+package br.maua.Atividade2.teste_drive;
+
+import br.maua.Atividade2.controlador.SistemaHorario;
+import br.maua.Atividade2.enums.HorarioSistema;
+import br.maua.Atividade2.models.Membro;
+
+import java.util.LinkedList;
+
+/**
+ * Classe para testar ideias antes de implementar.
+ */
+public class TesteDrive {
+    public static void main(String[] args){
+        SistemaHorario horario = new SistemaHorario(HorarioSistema.EXTRA);
+        LinkedList<Membro> membros = new LinkedList<>();
+
+/*
+        membros.add(new HeavyLifters("Max","Max@gmail.com", 01));
+        membros.add(new ScriptGuys("Mel","Mel@gmail.com", 02));
+        membros.add(new MobileMembers("Stella","Stella@gmail.com",03));
+        membros.add(new BigBrothers("G","G@M.com",00));
+*/
+
+        System.out.println("-------------------------------------------");
+        System.out.println("Passando por lista ligada");
+        membros.forEach(pessoa -> System.out.println(pessoa.apresentar()));
+        System.out.println("-------------------------------------------");
+        membros.forEach(pessoa -> pessoa.assinaMensagem(horario));
+
+        int id = membros.getFirst().getId();
+        System.out.println(id);
+    }
+}
