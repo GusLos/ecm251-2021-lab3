35e3e1085b9d615a77a863193f7a43883775e1ea
GusLos <79462583+GusLos@users.noreply.github.com>
2021-06-26 18:36:35 -0300

    Import de teste

--- Atividade2/Atividade2/src/br/maua/Atividade2/teste_drive/TesteDrive.java
+++ Atividade2/Atividade2/src/br/maua/Atividade2/teste_drive/TesteDrive.java
@@ -2,7 +2,7 @@ package br.maua.Atividade2.teste_drive;
 
 import br.maua.Atividade2.controlador.SistemaHorario;
 import br.maua.Atividade2.enums.HorarioSistema;
-import br.maua.Atividade2.models.Membro;
+import br.maua.Atividade2.models.*;
 
 import java.util.LinkedList;
 
@@ -14,12 +14,12 @@ public class TesteDrive {
         SistemaHorario horario = new SistemaHorario(HorarioSistema.EXTRA);
         LinkedList<Membro> membros = new LinkedList<>();
 
-/*
+
         membros.add(new HeavyLifters("Max","Max@gmail.com", 01));
         membros.add(new ScriptGuys("Mel","Mel@gmail.com", 02));
         membros.add(new MobileMembers("Stella","Stella@gmail.com",03));
         membros.add(new BigBrothers("G","G@M.com",00));
-*/
+
 
         System.out.println("-------------------------------------------");
         System.out.println("Passando por lista ligada");
