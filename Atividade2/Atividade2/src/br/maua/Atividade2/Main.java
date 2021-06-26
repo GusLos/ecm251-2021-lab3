b0b19de7b9692d6f1bb545a66985b8aabf5050b6
GusLos <79462583+GusLos@users.noreply.github.com>
2021-06-26 17:30:18 -0300

    Classe main chama sistema

--- Atividade2/Atividade2/src/br/maua/Atividade2/Main.java
+++ Atividade2/Atividade2/src/br/maua/Atividade2/Main.java
@@ -1,8 +1,11 @@
 package br.maua.Atividade2;
 
+import br.maua.Atividade2.controlador.Sistema;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Sistema sistema = new Sistema();
+        sistema.run();
     }
 }
