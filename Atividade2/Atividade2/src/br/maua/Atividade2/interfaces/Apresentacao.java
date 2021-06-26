884257275f6dfdf27378aa1b56f5a123bd53c032
GusLos <79462583+GusLos@users.noreply.github.com>
2021-06-26 17:38:27 -0300

    Interface Apresentação

--- Atividade2/Atividade2/src/br/maua/Atividade2/interfaces/Apresentacao.java
+++ Atividade2/Atividade2/src/br/maua/Atividade2/interfaces/Apresentacao.java
@@ -1,4 +1,13 @@
 package br.maua.Atividade2.interfaces;
 
+/**
+ * Interface que os membros devem implementar para poder se apresentar quando for necessario.
+ */
 public interface Apresentacao {
+
+    /**
+     * Método que Membros deve ter para poder se apresentar quando necessario.
+     * @return String contendo principais parâmetros(futuramente).
+     */
+    public String apresentar();
 }
