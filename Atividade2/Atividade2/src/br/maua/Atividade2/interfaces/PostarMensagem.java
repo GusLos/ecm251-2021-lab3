2ffee801e4b9e71d468c30b86fde535498c34753
GusLos <79462583+GusLos@users.noreply.github.com>
2021-06-26 17:41:31 -0300

    Interface PostarMensagem
    >No método eu sei que vou utilizar uma classe SistemaHorario pois estou passando de pouco em pouco da Atv2(antigo) para Atividade2

--- Atividade2/Atividade2/src/br/maua/Atividade2/interfaces/PostarMensagem.java
+++ Atividade2/Atividade2/src/br/maua/Atividade2/interfaces/PostarMensagem.java
@@ -1,4 +1,13 @@
 package br.maua.Atividade2.interfaces;
 
+/**
+ * Interface que os Membros devem implementar para poder postar sua mensagem.
+ */
 public interface PostarMensagem {
+
+    /**
+     * Método que Membros (e seus filhos) devem ter, para postar/assinar mensagem.
+     * @return print da mensagem especial de cada membro.
+     */
+    public void assinaMensagem(SistemaHorario horario);
 }
