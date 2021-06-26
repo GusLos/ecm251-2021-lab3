d1d155255cbadae3ef9ab9978c3b670525d478e2
GusLos <79462583+GusLos@users.noreply.github.com>
2021-06-26 09:37:02 -0300

    Classe abstrata Membros

---
+++ Atividade2/Atv2/src/losada/gustavo/models/Membros.java
@@ -0,0 +1,12 @@
+package losada.gustavo.models;
+
+import losada.gustavo.enums.TiposMembros;
+
+/**
+ * Classe abstrata Membros possui o que os membros tem em comum ().
+ */
+public abstract class Membros {
+    private String nome;
+    private String eMail;
+    private TiposMembros funcao;
+}
