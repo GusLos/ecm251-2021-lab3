e8732dde61ada752756851a95a48989851f23577
GusLos <79462583+GusLos@users.noreply.github.com>
2021-06-26 12:04:39 -0300

    Melhorias em Sistema
    >Método run() foi trocado;
    >Método menu() agora mostra as opções que o usuário pode escolher;
    >Método analisarOpcao(), que faz análise da opção do usuário para realizar uma ação específica.

--- Atividade2/Atv2/src/losada/gustavo/controlador/Sistema.java
+++ Atividade2/Atv2/src/losada/gustavo/controlador/Sistema.java
@@ -18,18 +18,48 @@ public class Sistema {
     }
 
     public void run(){
-        boolean op = true;
-        while(op){
-            System.out.println(horario.horarioAtual());
-            System.out.println("---------------------------");
-            horario.mudarHorario();
-            System.out.println(horario.horarioAtual());
-            op = false;
+        int opcao = 1;
+        while(opcao != 0){
+            opcao = menu();
+            analisarOpcao(opcao);
         }
     }
 
-    private void menu(){
+    /**
+     * Método que mostra o menu para o usuário para que ele selecione uma ação (opção).
+     * @return int op, sendo op a opção de acordo com o menu apresentado.
+     */
+    private int menu(){
+        int op = 1;
+        System.out.println("Bem vindo\t\t\t\tHorario atual: "+horario.horarioAtual());
+        System.out.println();
+        System.out.println("Escolha sua ação:");
+        System.out.println("0 - Sair do sistema;");
+        System.out.println("1 - Verificar horário atual;");
+        System.out.println("2 - Mudar horário atual;");
+        op = scanner.nextInt();
+        return op;
+    }
 
+    /**
+     * Método que avalia o parametro opcao (que é a opção do usuário), e realiza uma ação específica.
+     * @param opcao
+     */
+    private void analisarOpcao(int opcao){
+        switch (opcao){
+            case 0:
+                System.out.println("Se cuida, até mais.");
+                break;
+            case 1:
+                System.out.println("Horario atual do sistema: "+horario.horarioAtual());
+                break;
+            case 2:
+                horario.mudarHorario();
+                System.out.println("Horario mudado com sucesso para: "+horario.horarioAtual());
+                break;
+            default:
+                System.out.println("Opção invalida.");
+        }
     }
 
     private void registrarMembro(){}
