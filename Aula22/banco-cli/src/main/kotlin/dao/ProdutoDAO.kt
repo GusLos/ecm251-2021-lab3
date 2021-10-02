88ef173fcacc6ed6a01bc451de573cdecdbd5d6a
Gustavo <gu.losada@hotmail.com>
2021-10-02 18:38:06 -0300

    Aula22

--- Aula22/banco-cli/src/main/kotlin/dao/ProdutoDAO.kt
+++ Aula22/banco-cli/src/main/kotlin/dao/ProdutoDAO.kt
@@ -1,6 +1,7 @@
 package dao
 
 import models.Produto
+import shared.SharedPaths
 import java.sql.DriverManager
 
 class ProdutoDAO : GenericoDAO {
@@ -25,48 +26,111 @@ class ProdutoDAO : GenericoDAO {
             println("Produto encontrado: ${produto}")
         }
         resultSet.close()
+        sqlStatement.close()
         connection.close()
         return produto!!
     }
 
     override fun pegarTodos(): List<Any> {
         //Cria uma conexão com o banco
-        val connection = DriverManager.getConnection("jdbc:sqlite:meubanco.db")
-
-        //Cria um caminho para realizar busca/queries no banco
-        val sqlStatement = connection.createStatement()
-        //Executa uma query de busca
-        val resultSet = sqlStatement.executeQuery("SELECT * FROM produtos;")
-
+        val connection = ConnectionDAO()
+        val resultSet = connection.executeQuery("SELECT * FROM produtos;")
         //Itera pelo resultado obtido
         val produtos = mutableListOf<Produto>()
-        while(resultSet.next()){
+        while(resultSet?.next()!!){
             produtos.add(Produto(
                 resultSet.getInt("id"),
                 resultSet.getString("nome"),
                 resultSet.getDouble("valor"),
                 resultSet.getInt("quantidade")
             ))
-            println("Produto encontrado: ${produtos}")
         }
-        resultSet.close()
         connection.close()
         return produtos
     }
 
+    fun pegarTodosSeguro(): List<Any> {
+        val produtos = mutableListOf<Produto>()
+        var connection = ConnectionDAO() //prof fezdiferente aqui ver connection : ConnectionDAO
+        try {                               //linha "56" connection = ConectionDAO()
+            val resultSet = connection.executeQuery("SELECT * FROM produtos;")
+
+            while (resultSet?.next()!!) {
+                produtos.add(
+                    Produto(
+                        resultSet.getInt("id"),
+                        resultSet.getString("nome"),
+                        resultSet.getDouble("valor"),
+                        resultSet.getInt("quantidade")
+                    )
+                )
+            }
+        }
+        catch (exception:Exception){
+            exception.printStackTrace()
+        }
+        finally{
+            connection.close()
+        }
+        return produtos
+    }
+
     override fun inserirUm(objeto: Any) {
-        TODO("Not yet implemented")
+        val connectionDAO = ConnectionDAO()
+        val preparedStatement = connectionDAO.getPreparedStatement("""INSERT INTO produtos 
+            |(nome, valor, quantidade) 
+            |VALUES (?,?,?)""".trimMargin())
+        val produto = objeto as Produto
+        preparedStatement?.setString(1,objeto.nome)
+        preparedStatement?.setDouble(2,objeto.valor)
+        preparedStatement?.setInt(3,objeto.quantidade)
+        preparedStatement?.executeUpdate()
+        //connectionDAO.commit()    //Banco já está em auto-commit
+        connectionDAO.close()
     }
 
     override fun inserirVarios(lista: List<Any>) {
-        TODO("Not yet implemented")
+        val connectionDAO = ConnectionDAO()
+        val preparedStatement = connectionDAO.getPreparedStatement("""INSERT INTO produtos 
+            |(nome, valor, quantidade) 
+            |VALUES (?,?,?)""".trimMargin())
+        for (objeto in lista) {
+            val produto = objeto as Produto
+            preparedStatement?.setString(1, objeto.nome)
+            preparedStatement?.setDouble(2, objeto.valor)
+            preparedStatement?.setInt(3, objeto.quantidade)
+            preparedStatement?.executeUpdate()
+            //connectionDAO.commit()    //Banco já está em auto-commit
+        }
+        connectionDAO.close()
     }
 
     override fun atualizar(objeto: Any) {
-        TODO("Not yet implemented")
+        val connectionDAO = ConnectionDAO()
+        val preparedStatement = connectionDAO.getPreparedStatement("""
+            |UPDATE produtos 
+            |SET nome = ?, valor = ?, quantidade = ? 
+            |WHERE id = ?;
+            |""".trimMargin())
+        val produto = objeto as Produto
+        preparedStatement?.setString(1,produto.nome)
+        preparedStatement?.setDouble(2,produto.valor)
+        preparedStatement?.setInt(3,produto.quantidade)
+        preparedStatement?.setInt(4,produto.id)
+        preparedStatement?.executeUpdate()
+        //connectionDAO.commit()    //Banco já está em auto-commit
+        connectionDAO.close()
     }
 
     override fun deletar(id: Int) {
-        TODO("Not yet implemented")
+        val connectionDAO = ConnectionDAO()
+        val preparedStatement = connectionDAO.getPreparedStatement("""
+            |DELETE FROM produtos 
+            |WHERE id = ?;
+            |""".trimMargin())
+        preparedStatement?.setInt(1,id)
+        preparedStatement?.executeUpdate()
+        //connectionDAO.commit()    //Banco já está em auto-commit
+        connectionDAO.close()
     }
 }
--- Aula22/banco-cli/src/main/kotlin/main.kt
+++ Aula22/banco-cli/src/main/kotlin/main.kt
@@ -1,25 +1,25 @@
+import dao.ProdutoDAO
 import models.Produto
 import java.sql.DriverManager
 
 fun main(args: Array<String>) {
-    //Cria uma conexão com o banco
-    val connection = DriverManager.getConnection("jdbc:sqlite:meubanco.db")
+    //Cria um DAO para os produtos
+    val produtosDAO = ProdutoDAO()
+    var produtos = produtosDAO.pegarTodosSeguro()
+    //Intera pelo resultado obtido
+    //produtosDAO.inserirUm(Produto(0,"agua",5.0,12))
 
-    //Cria um caminho para realizar busca/queries no banco
-    val sqlStatement = connection.createStatement()
-    //Executa uma query de busca
-    val resultSet = sqlStatement.executeQuery("SELECT * FROM produtos;")//WHERE ...; ?
+    //produtosDAO.inserirVarios(listOf(
+    //    Produto(0,"Clementine",799.98,1),
+    //   Produto(0,"Listerine",15.99,80)))
 
-    //Itera pelo resultado obtido
-    while(resultSet.next()){
-        val produto = Produto(
-            resultSet.getInt("id"),
-            resultSet.getString("nome"),
-            resultSet.getDouble("valor"),
-            resultSet.getInt("quantidade")
-        )
-        println("Produto encontrado: ${produto}")
+    produtosDAO.deletar(7)
+    produtosDAO.atualizar(Produto(8,"Batata",1.95,50))
+    
+    produtos = produtosDAO.pegarTodosSeguro()
+    for (produto in produtos){
+        println(produto)
     }
-    resultSet.close()
-    connection.close()
+
+
 }
